a3047a3fd3aa47ae2b1b8775dd14f8a4b0d92597
Steph Bench <stephane.benchimol@mfi.fr>
2021-02-04 11:17:52 +0100

    Fix typo

--- laptop/s3_gfs_flow.py
+++ laptop/s3_gfs_flow.py
@@ -20,7 +20,7 @@ settings = {
     'timesteps': [0, 1, 2, 3, 4, 5, 6],
     'max_concurrent_download': 5,
     'download_dir': "/tmp/laptop/s3/gfs",
-    'post_flowrun': StartFlowRun(flow_name="gfs-post-processing", project_name=prefect_project_name)
+    #'post_flowrun': StartFlowRun(flow_name="gfs-post-processing", project_name=prefect_project_name)
 }
 
 # Create a prefect's flow object with some configuration
@@ -28,35 +28,23 @@ flow_nwp_00 = create_flow_download(run=00, **settings)
 flow_nwp_12 = create_flow_download(run=12, **settings)
 
 flow_list = [flow_nwp_00]
+for flow in flow_list:
+    # Configure how this code will be passed to the prefect agents
+    # In this case, prefect will get this file from github
+    flow.storage = GitHub(
+        repo="steph-ben/datafetch-config",  # name of repo
+        path="laptop/s3_gfs_flow.py",  # location of flow file in repo
+        secrets=["GITHUB_ACCESS_TOKEN"]  # name of personal access token secret
+    )
+
+    # Configure how this code will be executed
+    # In this case, prefect will run this inside a docker container
+    flow.run_config = DockerRun(
+        #image="stephben/datafetch"
+    )
 
 
 def main(cmd):
-    # This part of the flow configuration is only needed when registering the flow
-
-    # Configure how this code will be passed to the prefect agents
-    # In this case, a docker image containing current file and associated libraries
-    # will be created
-    fp = Path(__file__)
-    for flow in flow_list:
-        # flow.storage = Docker(
-        #     stored_as_script=True,
-        #     files={fp.absolute(): f"/flow/{fp.name}"},  # Copy current file to container
-        #     path=f"/flow/{fp.name}",  # Use this file for running the flow
-        #     python_dependencies=["git+https://github.com/steph-ben/datafetch"],
-        #     build_kwargs={'nocache': True}
-        # )
-        flow.storage = GitHub(
-                repo="stephben/datafetch-config",  # name of repo
-                path="laptop/s3_gfs_flow.py",  # location of flow file in repo
-                secrets=["2451054577d8ff030a2267853876b3a7e9ef0b42"]  # name of personal access token secret
-        )
-
-    # This flow will run only in docker
-    for flow in flow_list:
-        flow.run_config = DockerRun(
-            image="stephben/datafetch"
-        )
-
     if cmd in ("register", "trigger"):
         # Ensure the flow is well registered in prefect server
         for flow in flow_list:
