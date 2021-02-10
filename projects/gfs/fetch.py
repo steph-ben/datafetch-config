12806da1e7e649721e2a01c9a5ceeea9554db78c
Steph Bench <stephane.benchimol@mfi.fr>
2021-02-10 12:16:40 +0100

    Fix file structure

--- projects/gfs/fetch.py
+++ projects/gfs/fetch.py
@@ -1,21 +1,25 @@
 """
 GFS flow for downloading on MFI datahub
 """
-import sys
+import os
+import logging
 
 from prefect.engine.results import PrefectResult
 from prefect.run_configs import DockerRun
-from prefect.storage import GitHub, Docker
+from prefect.storage import GitHub
+from prefect.tasks.prefect import StartFlowRun
 
 from datafetch.s3.flows import create_flow_download
-from datafetch.utils import trigger_prefect_flow
-from prefect.tasks.prefect import StartFlowRun
+from datafetch.utils import show_prefect_cli_helper
+
+logger = logging.getLogger(__name__)
+
 
 prefect_project_name = "gfs"
 
 settings = {
     'flow_name': "fetch-gfs",
-    'timesteps': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 18, 21, 24],
+    'timesteps': list(range(0, 12)) + list(range(12, 96, 3)),
     'max_concurrent_download': 5,
     'download_dir': "/data/hub/in/NOAA_GFS_FULL",
     'post_flowrun': StartFlowRun(
@@ -28,14 +32,16 @@ settings = {
 flow_nwp_00 = create_flow_download(run=00, **settings)
 flow_nwp_12 = create_flow_download(run=12, **settings)
 
-flow_list = flow_nwp_00, flow_nwp_12
+flow_list = [flow_nwp_00, flow_nwp_12]
 for flow in flow_list:
     # Configure how this code will be passed to the prefect agents
     # In this case, prefect will get this file from github
+    repo_ref = os.getenv("DATAFETCH__STORAGE__REPO__REF", default="master")
+    print(f"Registering Using GitHub repo ref {repo_ref}")
     flow.storage = GitHub(
         repo="steph-ben/datafetch-config",  # name of repo
-        ref="master",
-        path="mfi_datahub/s3_gfs_flow.py",  # location of flow file in repo
+        ref=repo_ref,
+        path="projects/gfs/fetch.py",  # location of flow file in repo
         secrets=["GITHUB_ACCESS_TOKEN"]  # name of personal access token secret
     )
 
@@ -52,30 +58,5 @@ for flow in flow_list:
     flow.result = PrefectResult()
 
 
-def main(cmd):
-    if cmd in ("register", "trigger"):
-        # Ensure the flow is well registered in prefect server
-        for flow in flow_list:
-            r = flow.register(project_name=prefect_project_name, labels=["docker"])
-            print(r)
-
-    if cmd == "trigger":
-        # Trigger the flow manually
-        for flow in flow_list:
-            trigger_prefect_flow(
-                flow_name=flow.name,
-                run_name=f"{flow.name}-manually_triggered",
-            )
-
-    if cmd == "run":
-        # Run a download from current process
-        flow_nwp_00.schedule = None
-        flow_nwp_00.run()
-
-
 if __name__ == "__main__":
-    cmd = "register"
-    if len(sys.argv) > 1:
-        cmd = sys.argv[1]
-
-    main(cmd)
+    show_prefect_cli_helper(flow_list=flow_list)
