4b0f5ee6ad5c8d04f56b242ecda482dceab9d531
Stephane <stephane.benchimol@gmail.com>
2021-02-10 22:26:18 +0100

    Fix gfs/post_process.py (#3)
    * Fix file structure
    
    * Working on ci
    
    * Working on ci
    
    * Working on ci
    
    * Fix path
    
    * Fixing
    
    * Fixing
    
    * Fixing
    
    * Fixing
    
    Co-authored-by: Steph Bench <stephane.benchimol@mfi.fr>

--- projects/gfs/post_process.py
+++ projects/gfs/post_process.py
@@ -3,9 +3,7 @@ import os
 import prefect
 from prefect.engine.results import PrefectResult
 from prefect.run_configs import DockerRun
-from prefect.storage import GitHub, Docker
-
-from datafetch.utils import show_prefect_cli_helper
+from prefect.storage import GitHub
 
 
 @prefect.task
@@ -33,11 +31,12 @@ print(f"Registering Using GitHub repo ref {repo_ref}")
 flow.storage = GitHub(
     repo="steph-ben/datafetch-config",
     ref=repo_ref,
-    path="projects/gfs/post_processing.py",
+    path="projects/gfs/post_process.py",
     secrets=["GITHUB_ACCESS_TOKEN"]
 )
 flow.run_config = DockerRun()
 
 
 if __name__ == "__main__":
+    from datafetch.utils import show_prefect_cli_helper
     show_prefect_cli_helper(flow_list=[flow])
